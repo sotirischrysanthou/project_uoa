62e2eb2d3aa640719c0035a38de70b357b36548a
john-ioannides <ioannides.johnakos@gmail.com>
2020-11-14 01:28:19 +0200

    functions

--- include/functions.h
+++ include/functions.h
@@ -8,3 +8,8 @@
 #include "compare_funcs.h"
 #include "../include/hashtable.h"
 #include "compare_funcs.h"
+
+void foo(List main_l, List to_del_l);
+void print_list(List list_to_print);
+void print_commons(List visited_lists, List list_to_visit, FILE *output_file);
+HashTable read_all_folders(string dir_name);
